c68724dd3617d6ca057e6be807f096b78dbc3446
Mario Krstevski <mariokrstevski@hotmail.com>
2024-02-04 01:13:45 +0100

    add count in secondary nav for applications

--- src/route-setup/layouts/ApplicationsLayout.tsx
+++ src/route-setup/layouts/ApplicationsLayout.tsx
@@ -1,30 +1,60 @@
 import { Outlet } from "react-router-dom";
 import SecondaryNav from "@/components/SecondaryNav";
-
-const secondaryNavLinks = [
-  {
-    to: "/d/applications",
-    label: "Active",
-  },
-  {
-    to: "/d/applications/wishlist",
-    label: "Wishlist",
-  },
-  {
-    to: "/d/applications/archived",
-    label: "Archived",
-  },
-  {
-    to: "/d/applications/import",
-    label: "Import",
-  },
-  {
-    to: "/d/applications/kanban",
-    label: "Kanban Board",
-  },
-];
+import { useJobApplicationsStore } from "@/hooks/useJobApplicationsStore";
+import { useEffect, useState } from "react";
 
 export default function ApplicationsLayout() {
+  const jobApplicationsStore = useJobApplicationsStore();
+  const jobApplications = jobApplicationsStore.jobApplications;
+  const [counts, setCounts] = useState({
+    active: 0,
+    wishlist: 0,
+    archived: 0,
+    total: 0,
+  });
+  //Calculate counts
+  useEffect(() => {
+    let activeCount = 0;
+    let wishlistCount = 0;
+    let archivedCount = 0;
+    for (const ja of jobApplications) {
+      if (ja.isArchived) {
+        archivedCount++;
+      } else if (ja.status === "Wishlist") {
+        wishlistCount++;
+      } else if (ja.status !== "Wishlist" || !ja.isArchived) {
+        activeCount++;
+      }
+    }
+    setCounts({
+      active: activeCount,
+      wishlist: wishlistCount,
+      archived: archivedCount,
+      total: jobApplications.length,
+    });
+  }, [jobApplications]);
+  const secondaryNavLinks = [
+    {
+      to: "/d/applications",
+      label: `Active (${counts.active})`,
+    },
+    {
+      to: "/d/applications/wishlist",
+      label: `Wishlist (${counts.wishlist})`,
+    },
+    {
+      to: "/d/applications/archived",
+      label: `Archived (${counts.archived})`,
+    },
+    {
+      to: "/d/applications/import",
+      label: "Import",
+    },
+    {
+      to: "/d/applications/kanban",
+      label: "Kanban Board",
+    },
+  ];
   return (
     <div>
       <SecondaryNav links={secondaryNavLinks} />
