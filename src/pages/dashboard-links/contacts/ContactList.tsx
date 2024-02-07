09ef45a4d13209aeff2b09692912c1e39ab23697
Mario Krstevski <mariokrstevski@hotmail.com>
2024-02-07 21:46:59 +0100

    attach note to contact

--- src/pages/dashboard-links/contacts/ContactList.tsx
+++ src/pages/dashboard-links/contacts/ContactList.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Contact } from "@/types";
 import CreateContactSmartOverlay from "./components/modals/CreateContactSmartOverlay";
 import { useDialogControl } from "@/hooks/useDialogControl";
+import NoteForm from "@/components/NoteForm";
 
 interface ContactListProps {
   contacts: Contact[];
@@ -19,9 +20,17 @@ export default function ContactList({ contacts }: ContactListProps) {
 
       <Button onClick={handleCreateContact}>Add Contact</Button>
       {contacts.length === 0 && <div>No contacts</div>}
-      <div>
+      <div className="flex flex-col flex-wrap gap-2">
         {contacts.map((contact) => {
-          return <div key={contact.id}>{contact.name}</div>;
+          return (
+            <div
+              className="p-4 border shadow bg-slate-50 max-w-96"
+              key={contact.id}
+            >
+              <div>{contact.name}</div>
+              {contact.note && <NoteForm note={contact.note} />}
+            </div>
+          );
         })}
       </div>
     </div>
