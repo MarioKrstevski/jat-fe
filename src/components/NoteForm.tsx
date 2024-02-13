8ef54df048b0835103652287c0444a727184e589
Mario Krstevski <mariokrstevski@hotmail.com>
2024-02-13 14:04:05 +0100

    add  resource comments for note

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -6,6 +6,9 @@ import { toast } from "sonner";
 import { Button } from "./ui/button";
 import { useMutation } from "@tanstack/react-query";
 
+// style change example
+// https://i.stack.imgur.com/Tr98d.png
+//https://javascript.plainenglish.io/creating-rich-text-editor-for-react-app-a-comprehensive-guide-using-the-quill-library-ae5ead8a0d3a
 interface NoteFormProps {
   note: Note;
 }
